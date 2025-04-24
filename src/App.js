ab529dd94f668b1cb3a61e8ed65c64cea86bbb50
Pushkar <thakurpushkar1532@gmail.com>
2025-04-24 10:17:15 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [question,setQuestion] = useState('');
+  const [response,setResponse] = useState('');
+
+ const submitHandler = (e)=>{
+  e.preventDefault();
+  console.log(question)
+  axios.post('https://gemini-app-three.vercel.app/getResponse', {
+    question: question
+  })
+  
+  .then(res=>{
+    console.log(res.data.response)
+    setResponse(res.data.response)
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+ }
+
+ const speakHandler = ()=>{
+  const a = new SpeechSynthesisUtterance(response)
+  window.speechSynthesis.speak(a)
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='box'>
+        <div className='profile-pic'>
+          <img className='pic' alt='profile pic' src={require('../src/assets/Virat Kohli Net Worth (2024)_ His Lifestyle, Career, and Properties.jpeg')}/>
+        </div>
+        <p className='label'>Ask Anything</p>
+        <textarea onChange={(e)=>{setQuestion(e.target.value)}} />
+        <button onClick={submitHandler}>Send</button>
+      </div>
+      <div className='box'>
+      <div className='profile-pic'>
+          <img className='pic' alt='profile pic' src={require('../src/assets/gemini1.png')}/>
+        </div>
+        <p className='label'>Gemini</p>
+        <textarea value={response}/>
+        <button onClick={speakHandler}>Speak</button>
+      </div>
     </div>
   );
 }
